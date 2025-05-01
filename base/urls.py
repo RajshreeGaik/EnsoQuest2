6e3a4815d4b7212050734d79d5846a103625ba7f
AnujaG23 <anuja.nfufg22@sinhgad.edu>
2025-05-01 13:19:04 +0530

    Inbox and Blog Setup

--- base/urls.py
+++ base/urls.py
@@ -5,10 +5,12 @@ urlpatterns =[
     path('', views.home, name='home'),
     path('leaderboard', views.leaderboard_view, name='leaderboard'),
     path('dashboard', views.dashboard_view, name='dashboard'),
+    path('message/<int:id>', views.message_view, name='message'),
     path('resources', views.resources_view, name='resources'),
     path('about', views.about_view, name='about'),
     path('contact', views.contact_view, name='contact'),
     path('terms_conditions', views.terms_conditions_view, name='terms_conditions'),
     path('blogs', views.blogs_view, name='blogs'),
-    path('blogs/<str:blog_id>', views.blog_view, name='blog')
+    path('blogs/<str:blog_id>', views.blog_view, name='blog'),
+    path('search/users', views.search_users_view, name='search_users'),
 ]
