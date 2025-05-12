25825a56b7cefc05287f610168db77e2120271ca
AnujaG23 <anuja.nfufg22@sinhgad.edu>
2025-05-12 18:21:39 +0530

    testing

--- base/tests.py
+++ base/tests.py
@@ -1,3 +1,61 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Message
+from django.urls import reverse
 
-# Create your tests here.
+class MessageModelTest(TestCase):
+    # set up 
+    def setUp(self):
+        #create user
+        self.user = User.objects.create_user(username='testuser',password='testpass')  
+
+        # create a sample message 
+        self.message = Message.objects.create(
+            user=self.user,
+            subject='Message subject',
+            message='this is my message'
+
+        )      
+    # TEST creation
+    def test_message_creation(self):
+        self.assertEqual(self.message.user,self.user)
+        self.assertEqual(self.message.subject,'Message subject')
+        self.assertEqual(self.message.message,'this is my message')
+
+        self.assertFalse(self.message.is_read)
+
+        self.assertIsNotNone(self.message.created_at)
+    # str representation
+
+    def test_str_representation(self):
+        self.assertEqual(str(self.message),f"{self.user.username},{self.message.subject}")
+
+# message url test
+class MessageUrlTest(TestCase):
+    def setUp(self):
+        self.superuser = User.objects.create_superuser(username='superuser', password='testpass')
+        self.message = Message.objects.create(
+            user=self.superuser,
+            subject='Message subject',
+            message='this is my message'
+        )
+    
+    def test_message_url_superuser(self):
+        self.client.login(username='superuser', password='testpass')
+
+        # responce
+        url = reverse('message',args=[self.message.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_message_url_normaluser(self):
+        self.user = User.objects.create_user(username='testuser',password='testpass')
+        self.client.login(username='testuser', password='testpass')
+
+        # responce
+        url = reverse('message',args=[self.message.id])
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 302)
--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,108 @@
 from django.test import TestCase
+from .models import Quiz, Question, Choice, Category
+import pandas as pd
+import io
+from django.core.files.uploadedfile import SimpleUploadedFile
+from unittest.mock import patch
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class QuizModelTest(TestCase):
+    # set up
+    def setUp(self):
+        #caterogy
+        self.category = Category.objects.create(name='AI')
+
+        # create excel file
+        self.excel_file = io.BytesIO()
+        df = pd.DataFrame({
+            'Question': ['what is 2+2?','what is 3+5?'],
+            'A': ['1','2'],
+            'B': ['3','4'],
+            'C': ['5','8'],
+            'D': ['4','6'],
+            'Answer': ['D','C']
+        })
+        df.to_excel(self.excel_file, index=False, engine="openpyxl")
+        self.excel_file.seek(0)
+
+        self.uploaded_file = SimpleUploadedFile('test_quiz.xlsx', self.excel_file.read(), content_type ='application/vnd.ms-excel')
+        
+
+        # QUIZ
+        self.quiz = Quiz.objects.create(
+            title='Quiz title',
+            category=self.category,
+            description = 'Quiz desc',
+            quiz_file= self.uploaded_file
+        )
+    @patch('quiz.models.pd.read_excel')
+    def test_import_quiz_from_excel(self, mock_read_excel):
+        mock_df = pd.DataFrame({
+            'Question': ['what is 2+2?','what is 3+5?'],
+            'A': ['1','2'],
+            'B': ['3','4'],
+            'C': ['5','8'],
+            'D': ['4','6'],
+            'Answer': ['D','C']
+        })
+        
+        mock_read_excel.return_value = mock_df
+
+        self.quiz.save()
+
+        #question created or not
+        self.assertEqual(Question.objects.count(), 2)
+        self.assertEqual(Choice.objects.count(), 8)
+
+        question1 =Question.objects.get(text='what is 2+2?')
+        question2 =Question.objects.get(text='what is 3+5?')
+
+        self.assertEqual(Choice.objects.filter(question=question1).count(), 4)
+        self.assertEqual(Choice.objects.filter(question=question2).count(), 4)
+    
+    # str
+
+    # additions
+    def test_plural_quizzes(self):
+        self.assertEqual(str(Quiz._meta.verbose_name_plural), 'Tests')
+
+# templet test case
+
+class AllQuizTemplateTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.client.login(username='testuser', password='testpass')
+
+        self.category1 = Category.objects.create(name='Science')
+        self.category2 = Category.objects.create(name='History')
+
+        self.quiz1 = Quiz.objects.create(title='Quiz1', description='Desc 1', category=self.category1)
+        self.quiz2 = Quiz.objects.create(title='Quiz2', description='Desc 2', category=self.category2)
+
+    def test_all_quiz_template(self):
+        response = self.client.get(reverse('all_quiz'))
+
+        self.assertTemplateUsed(response, 'all-test.html')
+
+        self.assertIn('quizzes', response.context)
+        self.assertIn('categories', response.context)
+
+        self.assertContains(response, 'Quiz1')
+        self.assertContains(response, 'Quiz2')
+        self.assertContains(response, 'Science')
+        self.assertContains(response, 'History')
+
+    def test_no_quizzes(self):
+        Quiz.objects.all().delete()
+
+        response = self.client.get(reverse('all_quiz'))
+        
+        self.assertContains(response, 'There is no Test available for this category or search.')
+
+
+
+
+
+
