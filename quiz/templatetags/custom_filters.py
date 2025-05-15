53c8dd7b67dcce3f3d64e78e27808cc0836635eb
RajshreeGaik <rajshreeg71@gmail.com>
2025-05-15 10:51:54 +0530

    added bg img

--- quiz/templatetags/custom_filters.py
+++ quiz/templatetags/custom_filters.py
@@ -6,3 +6,10 @@ def subtract(value, arg):
     'return total question - correct question '
     return value - arg
 
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_selected(answers, question):
+    return answers.filter(question=question).first().selected_choice if answers else None
